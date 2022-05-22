bf06eeebef8b4aa411a36a406258f1768bb8c1f0
juliandunne1234 <julianmorandunne@gmail.com>
2022-05-22 17:28:40 +0000

    3 services provided to customer

--- run.py
+++ run.py
@@ -12,6 +12,31 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('corner_store')
 
-restock = SHEET.worksheet('restock')
-inventory = restock.get_all_values()
-print(inventory)
+def open_shop():
+    """
+    Provide 3 options to the user:
+    1) See stock inventory
+    2) Input customer order
+    3) Complete existing customer order
+    """
+    print("---CORNER STORE---")
+    print("Please choose number 1-3 to proceed")
+    print("1) Current stock inventory")
+    print("2) Create new Customer Order")
+    print("3) Execute existing costomer order")
+    option_sel = input("Enter: ")
+
+    if option_sel == "1":
+        print("Test 1")
+    elif option_sel == "2":
+        print("Test 2")
+    elif option_sel == "3":
+        print("Test 3")
+    else:
+        print("The shop does not provide this service")
+
+
+def main():
+    open_shop()
+
+main()
